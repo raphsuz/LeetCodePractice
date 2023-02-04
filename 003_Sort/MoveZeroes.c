f68adb553c9338ca28cb09e7c0d8b35a393ccf94
raphsuz <50951230+raphsuz@users.noreply.github.com>
2023-02-04 19:19:35 +0800

    update the new practice

---
+++ 003_Sort/MoveZeroes.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void MoveZeroes(int* nums, int numsSize){
+    // input nums: [0,1,0,3,12], numSize: 5
+    // no return but shall output [1, 3, 12, 0, 0]
+
+    // the condition to end up
+}
