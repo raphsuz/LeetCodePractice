f92f6957fbb05e4c5b785c62f690a8926ad8151e
raphsuz <50951230+raphsuz@users.noreply.github.com>
2023-02-08 19:05:35 +0800

    solve the best time to b&s stock with recurrent method

--- 003_TwoPointer/BestTimeToBuyAndSellStock.c
+++ 003_TwoPointer/BestTimeToBuyAndSellStock.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void MaxProfit(int* prices, int numsSize){
+int MaxProfit(int* prices, int pricesSize){
     // input prices: [7, 1, 5, 3, 6, 4], numSize: 6
     // return 7 (because you can buy $1 at 2nd day and sell it at $5 at 3rd day to earn $4, and buy $3 at 4th day and sell it at $6 at 5th day to earn $3, totally earn $7)
 
-    // 1st condition :the condition to end up
-    //                it is easy to check whether correct or not
-    
+    if (pricesSize <= 1) return 0;
 
+    int profit;
+    int max = 0;
+
+    // 1st condition: may we sell stocks at the last day?    
+    // 1) if the answer is Yes, the situation will become: [7, 1, 5, 3, 6, 4] <-> [7, 1, 5, 3, 6]
+    //                                                     [7, 1, 5, 3, 6, 4] -> MaxProfit(prices, 6)
+    //                                                     [7, 1, 5, 3, 6]    -> MaxProfit(prices, 5)
+
+    profit = MaxProfit(prices, pricesSize - 1);
+    if (profit > max) {
+        max = profit;
+    }
+
+    // 2)if the answer is No, the situation will become:                             b  s
+    //                                                  MAX([7, 1, 5, 3, 6]     +   [6, 4]) -> MaxProfit(prices, 5) + (4 - 6),  
+    //                                                     ([7, 1, 5, 3]        +   [3, 4]) -> MaxProfit(prices, 4) + (4 - 3),
+    //                                                     ([7, 1, 5]           +   [5, 4]) -> MaxProfit(prices, 3) + (4 - 5),
+    //                                                     ([7, 1]              +   [1, 4]) -> MaxProfit(prices, 2) + (4 - 1),
+    //                                                     ([7]                 +   [7, 4]) -> MaxProfit(prices, 1) + (4 - 7))
+
+    for (size_t i = 1; i <= pricesSize - 1; i++) {
+        profit = MaxProfit(prices, i) + prices[pricesSize - 1] - prices[i - 1];
+        if (profit > max) {
+            max = profit;
+        }
+    }   
+    return max;
 }
 
 int main(void) {    
-    int input[5] = {0, 1, 0, 3, 12};
-    int numsSize = 5;
-    MaxProfit(input, numsSize);
-    for (size_t k = 0; k < numsSize; k++){
-        printf("The %d element of array is %d!\n", k, input[k]);
-    }
+    int prices[6] = {7, 1, 5, 3, 6, 4};
+    int pricesSize = 6;
+    int profit = MaxProfit(prices, pricesSize);    
+    printf("The most profit earned in this week is $%d!\n", profit);
     
 }
