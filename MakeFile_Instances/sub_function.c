f2ec0aa81eb52704f84f37bfcdb61c8a12a3db5a
raphsuz <50951230+raphsuz@users.noreply.github.com>
2023-04-14 19:59:28 +0800

    Makefile Instance upload

---
+++ MakeFile_Instances/sub_function.c
@@ -0,0 +1,6 @@
+// sub_function.c
+#include <stdio.h>
+
+void sub_function() {
+    printf("Hello, World!\n");
+}
---
+++ MakeFile_Instances/sub_function.h
@@ -0,0 +1,7 @@
+// sub_function.h
+#ifndef SUB_FUNCTION_H
+#define SUB_FUNCTION_H
+
+void sub_function();
+
+#endif
